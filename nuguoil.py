7fcbe9442e43c18e93c2592af7a1c2ab9f853c53
hwi <jsh_mango_happy@naver.com>
2020-11-28 13:26:56 +0900

    patherrorclear

--- nuguoil.py
+++ nuguoil.py
@@ -240,7 +240,8 @@ class Getparams2(Resource):
         return response
 
 api.add_resource(Getparams,'/answer.lowprice.diesel','/answer.lowprice.gasoline','/answer.lowprice.diesel.0','/answer.lowprice.diesel.1','/answer.lowprice.gasoline.0','/answer.lowprice.gasoline.1','/answer.lowprice.select.diesel','/answer.lowprice.select.diesel0','/answer.lowprice.select.diesel1','/answer.lowprice.select.gasoline','/answer.lowprice.select.gasoline0','/answer.lowprice.select.gasoline1')
-api.add_resource(Getparams2,'/answer.lowprice','/')
+
+api.add_resource(Getparams2,'/answer.lowprice','/','/answer.lowprice.select')
 
 if __name__ == "__main__":
     app.run()
